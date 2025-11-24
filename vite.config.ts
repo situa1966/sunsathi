cffc1a8941fe4dc864859de3f234bc7add330b52
situa1966 <situa522@gmail.com>
2025-11-24 19:03:23 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(process.env.GEMINI_API_KEY),
+        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
       },
       resolve: {
         alias: {
